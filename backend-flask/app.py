67e0be9c787fad71f19ec6ef234991aab6883671
Kavali Vignesh sai Vishnu Kumar <kvsvishnukumar@gmail.com>
2023-03-03 19:25:49 +0000

    added rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -15,16 +15,6 @@ from services.show_activity import *
 from services.notifications_activities import *
 
 
-# # AWS X-RAY--------------
-# from aws_xray_sdk.core import xray_recorder
-# from aws_xray_sdk.core import patch_all
-# patch_all()
-
-# from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
-
-# xray_url = os.getenv("AWS_XRAY_URL")
-# xray_recorder.configure(service='backend-flask',dynamic_naming=xray_url)
-
 # Cloudwatch logs-----------
 import watchtower
 import logging
@@ -55,6 +45,13 @@ provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
+# Rollbar ------
+from time import strftime
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 # AWS X-RAY--------------
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.core import patch_all
@@ -83,6 +80,29 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+# Rollbar ----------
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+@app.route('/rollbar/test')
+def rollbar_test():
+  rollbar.report_message('Hello World','warning')
+  return "Hello world"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
