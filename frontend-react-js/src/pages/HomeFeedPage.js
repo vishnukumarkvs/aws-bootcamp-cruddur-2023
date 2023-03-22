77ca01f1124bf11b46d44f86c222d600cedaeffb
Kavali Vignesh sai Vishnu Kumar <kvsvishnukumar@gmail.com>
2023-03-22 06:57:54 +0000

    finished activities

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -97,7 +97,8 @@ const checkAuth = async () => {
     <article>
       <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
       <div className='content'>
-        <ActivityForm  
+        <ActivityForm
+          user_handle={user}  
           popped={popped}
           setPopped={setPopped} 
           setActivities={setActivities} 
