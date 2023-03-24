08490f7b73c77ede23fbf1cffa3d47b2744f319b
Kavali Vignesh sai Vishnu Kumar <kvsvishnukumar@gmail.com>
2023-03-24 19:43:13 +0000

    updatee-cognito users ids done

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,9 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown','anndrew@exampro.co', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko@exampro.co','bayko' ,'MOCK');
+  ('Bayko', 'bayko@exampro.co','bayko' ,'MOCK'),
+  ('Vishnu Kvs', 'kvs.vishnu23@gmail.com','Kavali Vignesh Sai' ,'MOCK'),
+  ('Vishnu Kumar', 'kvsvishnukumar@gmail.com','vvvv' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -35,13 +35,14 @@ class Db:
     for key, value in params.items():
       print(key, ":", value)
 
-  def print_sql(self,title,sql):
+  def print_sql(self,title,sql, params={}):
     cyan = '\033[96m'
     no_color = '\033[0m'
     print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
-    print(sql)
+    print(sql,params)
+    
   def query_commit(self,sql,params={}):
-    self.print_sql('commit with returning',sql)
+    self.print_sql('commit with returning',sql,params)
 
     pattern = r"\bRETURNING\b"
     is_returning_id = re.search(pattern, sql)
