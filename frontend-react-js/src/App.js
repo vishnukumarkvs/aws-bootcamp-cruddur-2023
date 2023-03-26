7e2575fdebee42e7ded19e355045ab284b95457f
Kavali Vignesh sai Vishnu Kumar <kvsvishnukumar@gmail.com>
2023-03-26 15:08:48 +0000

    messages done

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -63,7 +63,7 @@ const router = createBrowserRouter([
     element: <MessageGroupsPage />
   },
   {
-    path: "/messages/@:message_group_uuid",
+    path: "/messages/:message_group_uuid",
     element: <MessageGroupPage />
   },
   {
