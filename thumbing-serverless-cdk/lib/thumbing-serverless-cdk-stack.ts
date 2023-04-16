050413a6bf0411c9cee15d617712b282929c613e
Kavali Vignesh sai Vishnu Kumar <kvsvishnukumar@gmail.com>
2023-04-16 05:58:46 +0000

    added cdk with s3 bucket

---
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -0,0 +1,21 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class ThumbingServerlessCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+   const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+   const bucket = this.createBucket(bucketName)
+  }
+
+  createBucket(bucketName: string){
+    const bucket = new s3.Bucket(this,"ThumbingBucket",{
+      bucketName: bucketName,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+    return bucket;
+   }
+}
