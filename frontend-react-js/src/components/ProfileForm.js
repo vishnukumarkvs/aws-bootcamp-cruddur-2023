d630e2ab97a2c0a6efafcd4949452d0a51b875ee
Kavali Vignesh sai Vishnu Kumar <kvsvishnukumar@gmail.com>
2023-04-24 15:14:29 +0000

    figuring out

--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -17,8 +17,7 @@ export default function ProfileForm(props) {
     console.log('ext',extension)
     try {
       const gateway_url = `${process.env.REACT_APP_API_GATEWAY_ENDPOINT_URL}/avatars/key_upload`
-      await getAccessToken()
-      const access_token = localStorage.getItem("access_token")
+      const access_token = await getAccessToken()
       const json = {
         extension: extension
       }
